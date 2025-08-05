d149ecfd8de07a40da86c505c00b4d4f7eb5add0
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-08-05 12:06:04 +0600

    responsiveness issues fixed

--- src/components/card-container.tsx
+++ src/components/card-container.tsx
@@ -6,6 +6,7 @@ import { useGSAP } from '@gsap/react';
 import { useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import useIsMobile from "./isMobile";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -19,13 +20,14 @@ type CardProp = {
 
 export default function CardContainer() {
     const container = useRef<HTMLDivElement>(null)
+    const isMobile = useIsMobile()
     useGSAP(()=> {
 
         gsap.utils.toArray<HTMLElement>(".card-block").forEach((card)=> {
             gsap.to(card, {
                 scrollTrigger: {
                     trigger: card,
-                    start: "top 10%",
+                    start: isMobile ? "top 5%" : "top 10%",
                     end: "+=80%",
                     pin: true,
                     scrub: true,
@@ -41,12 +43,12 @@ export default function CardContainer() {
         <div ref={container} className='py-10 h-[450vh] container mx-auto relative'>
 
             {webProjects.map(blog => (
-                <div className="p-10 card-block flex items-center justify-between gap-8 bg-white h-[80vh] rounded-[45px] sticky top-10 -mb-[500px]">
+                <div key={blog.id} className="lg:p-10 p-3 card-block flex lg:items-center items-start justify-between gap-8 bg-white lg:h-[80vh] h-auto rounded-3xl lg:rounded-[45px] lg:flex-row flex-col-reverse sticky top-10 -mb-[400px]">
                     {/* information */}
-                    <div className="flex flex-col lg:gap-8 gap-2 items-start w-2/5">
-                        <h1 className="lg:text-7xl text-2xl font-medium">{blog.title}</h1>
-                        <div>
-                            {blog.category.map(cate => <Badge variant={"secondary"} className="px-4 py-2 m-2 rounded-full bg-background hover:bg-foreground hover:text-background outline outline-gray-300 hover:outline-none transition-all duration-300">{cate}</Badge>)}
+                    <div className="flex flex-col lg:gap-8 gap-2 items-start w-full lg:w-2/5">
+                        <h1 className="lg:text-7xl text-xl font-medium">{blog.title}</h1>
+                        <div className="flex flex-wrap items-center justify-start">
+                            {blog.category.map((cate,idx) => <Badge key={idx} variant={"secondary"} className="lg:px-4 lg:py-2 m-2 rounded-full bg-background hover:bg-foreground hover:text-background outline outline-gray-300 hover:outline-none transition-all duration-300">{cate}</Badge>)}
                         </div>
                         <p className="lg:text-xl text-muted-foreground text-sm">{blog.description}</p>
                         <MagneticButton distance={0.6}>
@@ -55,17 +57,12 @@ export default function CardContainer() {
                     </div>
 
                     {/* image */}
-                    <div className=" h-full w-3/5">
-                        <img src={`${blog.image}`} alt="image" className="w-full h-full object-cover rounded-tr-[200px]" />
+                    <div className=" h-full w-full lg:w-3/5">
+                        <img src={`${blog.image}`} alt="image" className="w-full h-full object-cover lg:rounded-tr-[200px] rounded-2xl " />
                     </div>
                 </div>
             ))}
 
-            {/* <div className="flex items-center justify-center">
-                <h1 className="text-7xl ">
-                    Thank you very much for the show
-                </h1>
-            </div> */}
         </div>
     )
 }
@@ -81,7 +78,7 @@ const webProjects: CardProp[] = [
     {
         id: 2,
         title: "Portfolio Website",
-        category: ["React", "Next.js", "TypeScript", "", "Tailwind", "Authentication"],
+        category: ["React", "Next.js", "TypeScript", "Tailwind", "Animation"],
         description: "A clean and modern personal portfolio website to showcase skills and projects.",
         image: "/MainAfter.avif"
     },
