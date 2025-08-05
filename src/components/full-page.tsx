1045c3df29c8ff80d34f78318d9ec8424af1ee42
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-08-05 12:06:17 +0600

    responsiveness solved

--- src/components/full-page.tsx
+++ src/components/full-page.tsx
@@ -1,7 +1,7 @@
 
 export default function FullPage({ title }: { title: string }) {
   return (
-    <section className={`text-7xl h-screen w-full text-black flex items-center justify-center`} >
+    <section className={`lg:text-7xl text-3xl h-screen w-full text-black flex items-center justify-center`} >
       {title}
     </section>
   )
